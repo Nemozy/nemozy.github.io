56dc3b2978ea7f43eb250dca3a5af067524edb05
cyberm8programmer <cyberm8programmer@gmail.com>
2017-03-12 21:36:16 +0300

    Build 0.20 Add photon server test
    Add photon server test

---
+++ Panzer batallion/Assets/Main assets/Scripts/Network/ConnectManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectManager : MonoBehaviour
+{
+    private string GameVersion = "ver 0.19";
+
+	void Start ()
+    {
+        PhotonNetwork.ConnectUsingSettings(GameVersion);
+	}
+
+    private void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    } 
+
+    void OnJoinedLobby()
+    {
+        Debug.Log("I JOINED IN LOBBY!");
+    }
+}
